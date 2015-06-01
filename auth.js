1304e534e8ab728b0fb5b13274086ecf849b4eba
yeojoey <yeojoey@gmail.com>
2015-06-02 02:30:31 +0800

    New methods getHouse and isOGL. students_info.json needs to be modified accordingly

--- auth.js
+++ auth.js
@@ -7,6 +7,14 @@ function isAllowed(phone) {
     return studentsInfo.hasOwnProperty(phone);
 }
 
+function getHouse(phone) {
+	return studentsInfo.phone.house;
+}
+
+function isOGL(phone) {
+	return studentsInfo.phone.ogl;
+}
+
 module.exports = {
     isAllowed: isAllowed
 };
